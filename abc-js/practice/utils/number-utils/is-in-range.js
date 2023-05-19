f3d7295400a64b31fed0bab4d673dfd9c89e5bb0
Nuradil <nuradilmaksut18@gmail.com>
2023-05-19 14:03:45 +0600

    Реализованы функций reverse, isInteger, isFloat, isInRangeTests

--- abc-js/practice/utils/number-utils/is-in-range.js
+++ abc-js/practice/utils/number-utils/is-in-range.js
@@ -1,4 +1,13 @@
 /** проверяет входит ли число num в диапозон от begin до end.
   Значения begin и end вхоодят в проверяемый диапазон.*/
 export function isInRange(num, begin, end) {
+  if (typeof(num) !== 'number'
+  || typeof(begin) !== 'number'
+  || typeof(end) !== 'number') {
+    throw Error('all parameter is required and must be number type');
+  }
+
+  if (num >= begin && num <= end) return true;
+
+  return false;
 }
--- abc-js/practice/utils/string-utils/reverse.js
+++ abc-js/practice/utils/string-utils/reverse.js
@@ -1,3 +1,10 @@
 /** Возвращает "развернутую" копию text */
 export function reverse(text) {
+    if (typeof(text) !== 'string') throw Error('argument must be type of string');
+
+    let reverseText = '';
+
+    for (let i = text.length - 1; i >= 0; i -= 1) reverseText += text[i];
+
+    return reverseText;
 }
