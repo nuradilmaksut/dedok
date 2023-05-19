f8135f50c6949335eb4457131d1759a0195d4d41
Nuradil-college <nuradilmaksut18@gmail.com>
2023-05-19 15:21:42 +0500

    Реализованы все модули по заданиям 1.10 до 1.14

--- abc-js/practice/utils/string-utils/repeat.js
+++ abc-js/practice/utils/string-utils/repeat.js
@@ -1,3 +1,16 @@
+import { isFloat } from "../number-utils/is-float.js";
+
 /** Возвращает text повторенный count раз. */
 export function repeat(text, count = 1) {
+    if (typeof(text) !== 'string') throw Error('argument must be type of string');
+
+    if (typeof(count) !== 'number'
+    || isFloat(count)
+    || count < 0) throw Error('invalid count');
+
+    let repeatText = '';
+
+    for (let i = 0; i < count; i += 1) repeatText += text;
+
+    return repeatText;
 }
--- abc-js/practice/utils/string-utils/substring.js
+++ abc-js/practice/utils/string-utils/substring.js
@@ -1,6 +1,28 @@
+import { isFloat } from '../number-utils/is-float.js';
+
 /** Возвращает копию text начиная с индекса start до индекса end.
  * Символ с индексом end не включается в выборку.
  * Если start не передано, то будет выборка начнется с первого символа.
  * Если end не передано, то будет возвращено text до последнего символа*/
-export function substring(text, start, end) {
+export function substring(text, start = 0, end) {
+    if (typeof(text) !== 'string') throw Error('argument must be type of string');
+
+    if (typeof(start) !== 'number'
+    || isFloat(start)
+    || start < 0) throw Error('invalid start index');
+
+    if (end === undefined) end = text.length;
+
+    if (typeof(end) !== 'number'
+    || text.length < end
+    || end < 0
+    || isFloat(end)) throw Error('invalid end index');
+
+    if (start > end) throw Error('invalid start and end index');
+
+    let substringText = '';
+
+    for( let i = start; i < end; i += 1) substringText += text[i];
+
+    return substringText; 
 }
