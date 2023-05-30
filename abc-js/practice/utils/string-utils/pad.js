f110be83880e36bfe8f97ad0f4bed58fb1bb8e13
Nuradil <nuradilmaksut18@gmail.com>
2023-05-30 23:05:27 +0600

    Реализованы функций padStart и padEnd в модуле pad.js

--- abc-js/practice/utils/string-utils/pad.js
+++ abc-js/practice/utils/string-utils/pad.js
@@ -1,13 +1,55 @@
+import { isNotNumber, isNotString } from "./common.js";
+import { len } from "./len.js";
+import { repeat } from "./repeat.js";
+
 /** Возвращает копию text увеличенный до длины maxLength
  * заполненный в начале символами fillString.
  * Допускается в fillString передавать строку из нескольких символов. */
-export function padStart(text, maxLength, fillString = ' ') {
+export function padStart(text, maxLength = 0, fillString = ' ') {
+    if (isNotString(text)) throw Error('argument text must be type of string');
+    if (isNotString(fillString)) throw Error('argument fillString must be type of string');
+
+    if (maxLength === null) return text;
+
+    if (isNotNumber(maxLength)) throw Error('invalid type of maxLength');
+
+    const fillingLength = maxLength - len(text);
+
+    const fillStringLength = len(fillString);
+    let additionalText = '';
+
+    if (maxLength < len(text)) return text;
+    
+    if (fillStringLength === 1) return repeat(fillString, fillingLength) + text;
+    
+    let fillStringIndex = 0;
+    for (let i = 0; i < fillingLength; i += 1) {
+        if (fillStringIndex === fillStringLength) fillStringIndex = 0;
+
+        additionalText += fillString[fillStringIndex];
+
+        fillStringIndex += 1;
+    }
+
+    return additionalText + text;
 }
 
 /** Возвращает копию text увеличенный до длины maxLength
  * заполненный в конце символами fillString.
  * Допускается в fillString передавать строку из нескольких символов. */
-export function padEnd(text, maxLength, fillString = ' ') {
+export function padEnd(text, maxLength = 0, fillString = ' ') {
+    if (isNotString(text)) throw Error('argument text must be type of string');
+    if (isNotString(fillString)) throw Error('argument fillString must be type of string');
+
+    if (maxLength === null) return text;
+    
+    if (isNotNumber(maxLength)) throw Error('invalid type of maxLength');
+
+    maxLength = maxLength - len(text);
+
+    fillString = padStart('', maxLength, fillString);
+
+    return text + fillString;
 }
 
 /** Возвращает копию text увеличенный до длины maxLength
--- abc-js/practice/utils/string-utils/pad.test.js
+++ abc-js/practice/utils/string-utils/pad.test.js
@@ -9,41 +9,41 @@ describe('pad Tests', () => {
       assertToBe(padStart('heh', 6), '   heh');
       assertToBe(padStart('right align', 50), ' '.repeat(39) + 'right align');
     });
-    // test('если длина совпадает, то вернется то же значение', () => {
-    //   assertToBe(padStart('hehe', 4), 'hehe');
-    // });
-    // test('если макс. длина меньше, то вернется то же значение', () => {
-    //   assertToBe(padStart('hehe', 0), 'hehe');
-    //   assertToBe(padStart('hehe', 3), 'hehe');
-    //   assertToBe(padStart('hehe', -7), 'hehe');
-    // });
-    // test('если макс. длина отсутствует, то вернется то же значение', () => {
-    //   assertToBe(padStart('hehe'), 'hehe');
-    //   assertToBe(padStart('hehe', null), 'hehe');
-    // });
-    // test('несоответствие типа второго параметра (строка), то вернется то же значение', () => {
-    //   assertThrow(() => padStart('hehe', 's'), 'invalid type of maxLength');
-    //   assertThrow(() => padStart('hehe', true), 'invalid type of maxLength');
-    // });
-    // test('другая строка заполнения', () => {
-    //   assertToBe(padStart('he', 4, '*'), '**he');
-    // });
-    // test('случай с длинной строкой заполнения', () => {
-    //   assertToBe(padStart('he', 6, 'Abcd'), 'Abcdhe');
-    // });
-    // test('строка заполнения не кратен вставке (неполная)', () => {
-    //   assertToBe(padStart('he', 5, 'Abcd'), 'Abche');
-    // });
-    // test('строка заполнения не кратен вставке (полная + неполная)', () => {
-    //   assertToBe(padStart('he', 8, 'Abcd'), 'AbcdAbhe');
-    // });
-    // test('строка заполнения не строковая, пирводит к приведению типа', () => {
-    //   assertThrow(() => padStart('he', 8, true), 'argument fillString must be type of string');
-    // });
-    // test('проверка типов первого аргумента', () => {
-    //   assertThrow(() => padStart(), 'argument text must be type of string');
-    //   assertThrow(() => padStart(true), 'argument text must be type of string');
-    // });
+    test('если длина совпадает, то вернется то же значение', () => {
+      assertToBe(padStart('hehe', 4), 'hehe');
+    });
+    test('если макс. длина меньше, то вернется то же значение', () => {
+      assertToBe(padStart('hehe', 0), 'hehe');
+      assertToBe(padStart('hehe', 3), 'hehe');
+      assertToBe(padStart('hehe', -7), 'hehe');
+    });
+    test('если макс. длина отсутствует, то вернется то же значение', () => {
+      assertToBe(padStart('hehe'), 'hehe');
+      assertToBe(padStart('hehe', null), 'hehe');
+    });
+    test('несоответствие типа второго параметра (строка), то вернется то же значение', () => {
+      assertThrow(() => padStart('hehe', 's'), 'invalid type of maxLength');
+      assertThrow(() => padStart('hehe', true), 'invalid type of maxLength');
+    });
+    test('другая строка заполнения', () => {
+      assertToBe(padStart('he', 4, '*'), '**he');
+    });
+    test('случай с длинной строкой заполнения', () => {
+      assertToBe(padStart('he', 6, 'Abcd'), 'Abcdhe');
+    });
+    test('строка заполнения не кратен вставке (неполная)', () => {
+      assertToBe(padStart('he', 5, 'Abcd'), 'Abche');
+    });
+    test('строка заполнения не кратен вставке (полная + неполная)', () => {
+      assertToBe(padStart('he', 8, 'Abcd'), 'AbcdAbhe');
+    });
+    test('строка заполнения не строковая, пирводит к приведению типа', () => {
+      assertThrow(() => padStart('he', 8, true), 'argument fillString must be type of string');
+    });
+    test('проверка типов первого аргумента', () => {
+      assertThrow(() => padStart(), 'argument text must be type of string');
+      assertThrow(() => padStart(true), 'argument text must be type of string');
+    });
   });
 
   describe('padEndTests', () => {
@@ -52,41 +52,41 @@ describe('pad Tests', () => {
       assertToBe(padEnd('heh', 6), 'heh   ');
       assertToBe(padEnd('left align', 50), 'left align' + ' '.repeat(40));
     });
-    // test('если длина совпадает, то вернется то же значение', () => {
-    //   assertToBe(padEnd('hehe', 4), 'hehe');
-    // });
-    // test('если макс. длина меньше, то вернется то же значение', () => {
-    //   assertToBe(padEnd('hehe', 0), 'hehe');
-    //   assertToBe(padEnd('hehe', 3), 'hehe');
-    //   assertToBe(padEnd('hehe', -7), 'hehe');
-    // });
-    // test('если макс. длина отсутствует, то вернется то же значение', () => {
-    //   assertToBe(padEnd('hehe'), 'hehe');
-    //   assertToBe(padEnd('hehe', null), 'hehe');
-    // });
-    // test('несоответствие типа второго параметра (строка), то вернется то же значение', () => {
-    //   assertThrow(() => padEnd('hehe', 's'), 'invalid type of maxLength');
-    //   assertThrow(() => padEnd('hehe', true), 'invalid type of maxLength');
-    // });
-    // test('другая строка заполнения', () => {
-    //   assertToBe(padEnd('he', 4, '*'), 'he**');
-    // });
-    // test('случай с длинной строкой заполнения', () => {
-    //   assertToBe(padEnd('he', 6, 'Abcd'), 'heAbcd');
-    // });
-    // test('строка заполнения не кратен вставке (неполная)', () => {
-    //   assertToBe(padEnd('he', 5, 'Abcd'), 'heAbc');
-    // });
-    // test('строка заполнения не кратен вставке (полная + неполная)', () => {
-    //   assertToBe(padEnd('he', 8, 'Abcd'), 'heAbcdAb');
-    // });
-    // test('строка заполнения не строковая, пирводит к приведению типа', () => {
-    //   assertThrow(() => padEnd('he', 8, true), 'argument fillString must be type of string');
-    // });
-    // test('проверка типов первого аргумента', () => {
-    //   assertThrow(() => padEnd(), 'argument text must be type of string');
-    //   assertThrow(() => padEnd(true), 'argument text must be type of string');
-    // });
+    test('если длина совпадает, то вернется то же значение', () => {
+      assertToBe(padEnd('hehe', 4), 'hehe');
+    });
+    test('если макс. длина меньше, то вернется то же значение', () => {
+      assertToBe(padEnd('hehe', 0), 'hehe');
+      assertToBe(padEnd('hehe', 3), 'hehe');
+      assertToBe(padEnd('hehe', -7), 'hehe');
+    });
+    test('если макс. длина отсутствует, то вернется то же значение', () => {
+      assertToBe(padEnd('hehe'), 'hehe');
+      assertToBe(padEnd('hehe', null), 'hehe');
+    });
+    test('несоответствие типа второго параметра (строка), то вернется то же значение', () => {
+      assertThrow(() => padEnd('hehe', 's'), 'invalid type of maxLength');
+      assertThrow(() => padEnd('hehe', true), 'invalid type of maxLength');
+    });
+    test('другая строка заполнения', () => {
+      assertToBe(padEnd('he', 4, '*'), 'he**');
+    });
+    test('случай с длинной строкой заполнения', () => {
+      assertToBe(padEnd('he', 6, 'Abcd'), 'heAbcd');
+    });
+    test('строка заполнения не кратен вставке (неполная)', () => {
+      assertToBe(padEnd('he', 5, 'Abcd'), 'heAbc');
+    });
+    test('строка заполнения не кратен вставке (полная + неполная)', () => {
+      assertToBe(padEnd('he', 8, 'Abcd'), 'heAbcdAb');
+    });
+    test('строка заполнения не строковая, пирводит к приведению типа', () => {
+      assertThrow(() => padEnd('he', 8, true), 'argument fillString must be type of string');
+    });
+    test('проверка типов первого аргумента', () => {
+      assertThrow(() => padEnd(), 'argument text must be type of string');
+      assertThrow(() => padEnd(true), 'argument text must be type of string');
+    });
   });
 
   describe('padTests', () => {
